4ad9aa95d3bbda05e6054c62511593f2af45553d
Franco Piemontesi <franco219011@gmail.com>
2024-10-18 17:33:05 -0300

    formArray implementation convertion to unTypedForms

--- src/app/match-form/match-form.component.ts
+++ src/app/match-form/match-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, inject, OnDestroy, Output } from '@angular/core';
 import { Match } from '../models/match';
-import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule, UntypedFormArray, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { DatePipe, JsonPipe } from '@angular/common';
 import { MatchService } from '../services/match.service';
 import { VisualizerService } from '../services/visualizer.service';
@@ -19,21 +19,39 @@ export class MatchFormComponent implements OnDestroy {
   today = new Date();
   @Output() onSave = new EventEmitter();
   subscription = new Subscription();
-  form = new FormGroup({
-    local: new FormControl("", [Validators.required]),
-    visitor: new FormControl("", [Validators.required]),
-    localScore: new FormControl(0, [Validators.required]),
-    visitorScore: new FormControl(0, [Validators.required, Validators.min(4)]),
-    date: new FormControl()
-  })
+  form = new UntypedFormGroup({
+    local: new UntypedFormControl("", [Validators.required]),
+    visitor: new UntypedFormControl("", [Validators.required]),
+    localScore: new UntypedFormControl(0, [Validators.required]),
+    visitorScore: new UntypedFormControl(0, [Validators.required, Validators.min(4)]),
+    date: new UntypedFormControl(),
+    events: new UntypedFormArray([])
+  });
 
   private matchService = inject(MatchService);
   private visualizerService = inject(VisualizerService);
   private readonly matchApiService = inject(MatchApiService);
   private readonly router = inject(Router);
 
+  get events() {
+    return this.form.controls["events"] as UntypedFormArray;
+  }
+
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  onNewEvent() {
+    const formArray = this.form.controls["events"] as FormArray;
+    const eventForm = new FormGroup({
+      minutes: new FormControl("", [Validators.required]),
+      seconds: new FormControl("", [Validators.required])
+    });
+    formArray.push(eventForm);
+  }
+
+  onDeleteEvent(index: number) {
+    this.events.removeAt(index);
   }
 
   save() {
@@ -48,7 +66,7 @@ export class MatchFormComponent implements OnDestroy {
     //   ...this.match
     // }
     // this.matchService.add(copyMatch);
-    const match = this.form.value as Match;
+    const match: Match = this.form.value;
     const addSubscription = this.matchApiService.add(match).subscribe({
       next: () => {
         this.router.navigate(['list']);
